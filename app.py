03e3a11a3e7e8dd365b45ff52177506932b4c9ea
Swastik07822 <116582848+Swastik07822@users.noreply.github.com>
2023-02-13 23:26:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,8 @@
 from flask import Flask, app, render_template, url_for, request
-import time 
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from E import get_weather
 import asyncio
 app = Flask(__name__)
 
-'''@app.route("/")
-def home():
-    #if request.method == 'POST':
-        #place =  request.form.get('City')
-        #print(result)
-    return render_template('home.html')'''
-	#return render_template('home.html')
 @app.route('/', methods = ['POST','GET'])
 def predict():
     if request.method == 'POST':
